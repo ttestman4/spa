9c73ee16cb6ac910febd09488a93b0cc028d7619
Test Man <ttestman4@gmail.com>
2018-03-30 23:23:19 +0530

    tsconfig noimplicit true

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,10 @@ if (environment.production) {
 
 const bootstrap = () => platformBrowserDynamic()
   .bootstrapModule(AppModule);
-
 if (environment.hmr) {
-  if (module[ 'hot' ]) {
-    hmrBootstrap(module, bootstrap);
+  const modulethis: any = module;
+  if (modulethis['hot']) {
+    hmrBootstrap(modulethis, bootstrap);
   } else {
     console.error('HMR is not enabled for webpack-dev-server!');
     console.log('Are you using the --hmr flag for ng serve?');
